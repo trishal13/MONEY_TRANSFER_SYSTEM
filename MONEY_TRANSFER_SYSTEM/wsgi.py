d6de3f69e0a432a868b6946fd1fb4db7449f7301
Trishal Makhija <134083369+trishal13@users.noreply.github.com>
2024-05-08 23:00:52 +0530

    code commit

---
+++ MONEY_TRANSFER_SYSTEM/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for MONEY_TRANSFER_SYSTEM project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MONEY_TRANSFER_SYSTEM.settings')
+
+application = get_wsgi_application()
+
+app = application
